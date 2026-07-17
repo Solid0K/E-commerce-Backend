10f39ba1f168ad9465c5c31ab269bd91dcb3498f
Solid0K <krisu.0396@gmail.com>
2026-07-17 14:51:04 +0530

    small change

--- src/main/java/com/krishu/ecommerce/Configuration/SecurityConfiguration.java
+++ src/main/java/com/krishu/ecommerce/Configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.krishu.ecommerce.Configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,7 +18,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
-@Component
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
